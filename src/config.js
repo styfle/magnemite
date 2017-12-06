25fcb8d4e7684245461bacb8e7984e17ab48dc48
Steven <steven@ceriously.com>
2017-12-06 13:13:06 -0500

    Fix config and bootstrap styles

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.SERVER_HOST = 'localhost';
 exports.SERVER_PORT = 8080;
-exports.WEBVIEW_START_PAGE = 'http://ceriously.dev';
+exports.WEBVIEW_START_PAGE = 'https://www.ceriously.com';
 //# sourceMappingURL=config.js.map
