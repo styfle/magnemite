cddc24735eba54d03fabae9283e09fbddbeca71e
Steven <steven@ceriously.com>
2018-09-23 15:13:41 -0400

    Remove js output

--- src/config.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SERVER_HOST = 'localhost';
-exports.SERVER_PORT = 8080;
-exports.WEBVIEW_START_PAGE = 'https://www.ceriously.com';
-//# sourceMappingURL=config.js.map
